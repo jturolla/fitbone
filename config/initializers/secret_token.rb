e32d555681f2d594973322878186881024916faf
Julio Turolla <julio.turolla@gmail.com>
2013-07-10 18:55:07 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitbone::Application.config.secret_key_base = '83691cf8bb571fa510fecf2a7eae084851299221a7a28112126d30ec759f4dd8e37c0d8366d87fa3cbd1677b6af59553b241501cd53e4d114ca58492f82e877b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fitbone::Application.config.session_store :cookie_store, key: '_fitbone_session'
